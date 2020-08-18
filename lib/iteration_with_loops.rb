9306c562d2971a598acb3a7e07aeef49b626a2f0
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-18 19:20:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,12 +5,9 @@ def find_min_in_nested_arrays(src)
     element_index = 0 
     temperature_minimum = src[row_index]{element_index}.min  
     while element_index < src[row_index].count do
-      
-      
      if src[row_index][element_index] == temperature_minimum
      min_temperature << src[row_index][element_index]
          end
-         
     element_index +=1 
   end
   row_index +=1 
